6a9087cef531ae3b887208485f11625d2a612cea
Fredrick Abayie <fredrick.abayie@gmail.com>
2015-10-10 03:23:05 +0000

    Trying to do the interface

---
+++ www/js/handlers.js
@@ -0,0 +1,20 @@
+/*global $*/
+
+//$(".button-collapse").;
+
+$(document).ready(function(){
+   alert("text"); 
+});
+
+
+//$(".button-collapse").sideNav(
+//    {
+//        menuWidth: 300,
+//        edge: 'right',
+//        closeOnClick: true
+////        $(".button-collapse").sideNav('show');
+//    }
+//);
+
+$(".button-collapse").sideNav('show');
+$(".button-collapse").sideNav('hide');

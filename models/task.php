c47d45ba73d6e1cb029ebd2f1fae0ac8608bff11
davidtandoh <tandohdavid@gmail.com>
2015-11-15 18:23:00 +0000

    First commit- Task class and getTaskforNurse function
    Function to get tasks assigned to a particular nurse. Only parameter
    for the function is the id of the nurse.

---
+++ models/task.php
@@ -0,0 +1,22 @@
+<?php
+	include_once ( "adb.php" );
+class task extends adb
+{	
+	/**
+	* Constructor for task
+	**/
+	function task (){}
+	
+	/**
+	* A function to get the tasks for a particular nurse given her id
+	**/
+    
+    
+    function getTaskForNurse($nurseid){
+        $select_query = "select task_id, task_title,task_description, task_start_date, task_end_date, name from nurses, tasks where tasks.nurseid = $nurseid and tasks.nurseid = nurse.nurseid";
+        return $this->query( $select_query );
+    }
+
+}
+
+?>

512574888748e017b5dbadaadbdddeffbe78c352
MD747 <mday747@gmail.com>
2015-11-25 21:35:19 +0000

    sql query bug fix

--- models/task.php
+++ models/task.php
@@ -3,7 +3,7 @@ include("adb.php");
 
 class task extends adb{
 	function searchTaskByName($taskName){
-		$str_query = "SELECT *  from task where task_title like '%$taskName%'";
+		$str_query = "SELECT *  from tasks where task_title like '%$taskName%'";
 		if (!$this->query($str_query )){
 			return false;
 		}

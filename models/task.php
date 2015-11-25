c02fe3f11b4943db9861ca9b5374039d50c21228
MD747 <mday747@gmail.com>
2015-11-25 17:43:42 +0000

    Added a function to search task by name
    This function searches for tasks assigned to nurses by the task name

--- models/task.php
+++ models/task.php
@@ -2,5 +2,13 @@
 include("adb.php");
 
 class task extends adb{
+	function searchTaskByName($taskName){
+		$str_query = "SELECT *  from task where task_title like '%$taskName%'"
+		if (!$this->query($str_query )){
+			echo'{"result":0, "message": "Error Searching Task'.mysql_error().'"}';
+			return false;
+		}
+		return true;
+	}
 }
 ?>

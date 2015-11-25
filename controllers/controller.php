5909b78703e449367655ed01839ce1f03ca982dc
MD747 <mday747@gmail.com>
2015-11-25 20:54:38 +0000

    Added more code to search task by name
    Added the function to instantiate the task class and execute necessary
    activities, also upon success A jSON array is returned
    
    Also the switch statement was added to check for commands and call the
    appropriate function.

--- controllers/controller.php
+++ controllers/controller.php
@@ -1,6 +1,38 @@
 <?php
-include("task.php");
+include("../models/task.php");
 if (!isset($_REQUEST['cmd'])){
 	echo '{"result" : 0, "message" : "Unset Comand"}';
+	exit();
 }
+switch ($cmd) {
+	case 1 : 
+		if (!isset($_REQUEST['tn'])){
+			echo '{"result" : 0, "message" : "Unset parameter"}';
+		} else {
+			searchTaskByName($_REQUEST['tn']);
+		}
+		break;
+	
+	default:
+		echo '{"result" : 0, "message" : "Command Invalid"}';
+		break;
+}
+function searchTaskByName($taskName){
+	$task = new task();
+	if (!$task->searchTaskByName($taskName)){
+		echo'{"result":0, "message": "Error Searching Task'.mysql_error().'"}';
+		return;
+	}
+	$row = $task->fetch();
+	echo '{"result" : 1, "message" : [';
+	while ($row){
+		echo json_decode($row)
+		if($row){
+			echo ",";
+		}
+	} 
+	echo ']}'
+	return;
+}
+
 ?>
--- models/task.php
+++ models/task.php
@@ -5,7 +5,6 @@ class task extends adb{
 	function searchTaskByName($taskName){
 		$str_query = "SELECT *  from task where task_title like '%$taskName%'"
 		if (!$this->query($str_query )){
-			echo'{"result":0, "message": "Error Searching Task'.mysql_error().'"}';
 			return false;
 		}
 		return true;

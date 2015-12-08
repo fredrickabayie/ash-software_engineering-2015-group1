623f97d44cfcfc38ca8a7d08b55bfe6459c818dc
Fredrick Abayie <fredrick.abayie@gmail.com>
2015-12-08 15:49:30 +0000

    Function for view of tasks implemented

--- controllers/php/archiveTask.php
+++ controllers/php/archiveTask.php
@@ -1,8 +1,33 @@
 <?php
 
-include_once '../';
-    class Archive {
+include_once 'adb.php';
 
 
+    class Archive extends adb {
 
-}
+        //Function to open or establish a connection
+        function __construct()
+        {
+            parent::__construct();
+        }
+
+        //Function to close the mysql connection
+        function __destruct()
+        {
+            parent::__destruct();
+        }
+
+        /**
+         * Function to view all the tasks
+         *
+         * @param $nurseId The id of the nurse
+         * @return bool Returns true when data is found and false otherwise
+         */
+        function viewTasks ( $nurseId ) {
+            $viewQuery = "SELECT *
+                          FROM `system_tasks`
+                          WHERE `system_tasks.user_id`='$nurseId'";
+            return $this->query($viewQuery);
+        }
+
+    }
